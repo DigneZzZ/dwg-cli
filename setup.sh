f4c29c74e9f24aaec2d07e70d858e57e32d9aa07
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-29 09:05:26 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Получаем внешний IP-адрес
-MYHOST_IP=$(hostname -I | cut -d' ' -f1)
 # Обновление пакетов
 printf "\e[42mОбновление пакетов...\e[0m\n"
 apt update
@@ -12,27 +10,27 @@ apt install git -y
 printf "\e[42mGit успешно установлен.\e[0m\n"
 
 # Клонирование репозитория
-printf "\e[42mКлонирование репозитория ad-wireguard...\e[0m\n"
-git clone https://github.com/dignezzz/ad-wireguard.git temp
+printf "\e[42mКлонирование репозитория dwg-cli...\e[0m\n"
+git clone https://github.com/dignezzz/dwg-cli.git temp
 
-if [ ! -d "ad-wireguard" ]; then
+if [ ! -d "dwg-cli" ]; then
   mkdir ad-wireguard
-  echo "Папка ad-wireguard создана."
+  echo "Папка dwg-cli создана."
 else
-  echo "Папка ad-wireguard уже существует."
+  echo "Папка dwg-cli уже существует."
 fi
 
 # копирование содержимого временной директории в целевую директорию с перезаписью существующих файлов и папок
-cp -rf temp/* ad-wireguard/
+cp -rf temp/* dwg-cli/
 
 # удаление временной директории со всем ее содержимым
 rm -rf temp
-printf "\e[42mРепозиторий ad-wireguard успешно клонирован до актуальной версии в репозитории.\e[0m\n"
+printf "\e[42mРепозиторий dwg-cli успешно клонирован до актуальной версии в репозитории.\e[0m\n"
 
 # Переходим в папку ad-wireguard
-printf "\e[42mПереходим в папку ad-wireguard...\e[0m\n"
-cd ad-wireguard
-printf "\e[42mПерешли в папку ad-wireguard\e[0m\n"
+printf "\e[42mПереходим в папку dwg-cli...\e[0m\n"
+cd dwg-cli
+printf "\e[42mПерешли в папку dwg-cli\e[0m\n"
 
 # Установка прав на файл установки
 printf "\e[42mУстановка прав на файл установки...\e[0m\n"
@@ -40,9 +38,9 @@ chmod +x install.sh
 printf "\e[42mПрава на файл установки выставлены.\e[0m\n"
 
 # Запуск установки
-printf "\e[42mЗапуск установки ad-wireguard...\e[0m\n"
+printf "\e[42mЗапуск установки dwg-cli...\e[0m\n"
 ./install.sh
-printf "\e[42mУстановка ad-wireguard успешно завершена.\e[0m\n"
+printf "\e[42mУстановка dwg-cli успешно завершена.\e[0m\n"
 
 # Установка прав на директорию tools
 printf "\e[42mУстановка прав на директорию tools...\e[0m\n"
@@ -66,9 +64,5 @@ printf "\e[42mПерешли в папку /root/ \e[0m\n"
 
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
 printf '\e[48;5;202m\e[30m Всё установлено! \e[0m\n'
-printf "\e[48;5;202m\e[30m Адрес входа в веб-интерфейс WireGuard после установки: http://$MYHOST_IP:51821 \e[0m\n"
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
-printf '\e[48;5;202m\e[30m Не забудь отдельно установить UFW-Docker, для закрытия веб-интерфейса wireguard. \e[0m\n'
-printf '\e[48;5;196m\e[97m ВНИМАНИЕ! Запускать только после того как создадите для себя клиента в WireGUARD!!! \e[0m\n'
-printf '\e[48;5;202m\e[30m команда для установки: ./ad-wireguard/tools/ufw-docker.sh \e[0m\n'
-printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
+
